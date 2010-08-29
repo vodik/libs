be7569e75825e661de48f1e0aee27b02fcaf04e7
simongmzlj <simongmzlj@gmail.com>
2010-08-29 05:02:15 -0400

    io is now referenced counted and the closing of a io will cleanup memory used to track callbacks for cleanup

--- io/backends/epoll.h
+++
@@ -1,8 +0,0 @@
-#ifndef NMD_BACKEND_EPOLL
-#define NMD_BACKEND_EPOLL
-
-#include "../watch.h"
-
-struct io_backend *epoll_backend_get();
-
-#endif
--- io/backends/select.h
+++
@@ -1,8 +0,0 @@
-#ifndef NMD_BACKEND_SELECT
-#define NMD_BACKEND_SELECT
-
-#include "../watch.h"
-
-struct io_backend *select_backend_get();
-
-#endif
